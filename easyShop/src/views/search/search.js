c2be9762cd87abf5d6bc65ca9f0739fd1019f933
gengzeng <1041355591@qq.com>
2019-07-27 10:21:31 +0800

    10.21

--- easyShop/src/views/search/search.js
+++ easyShop/src/views/search/search.js
@@ -5,18 +5,25 @@ class search extends Component {
         return (
             <div className='search_wrap'>
                 <div className="search_header">
-                    <p onClick={() => window.history.go(-1)} className='iconfont icon-zuo' style={{ width: '1rem', textAlign: 'center' }}></p>
-                    <p><input type="text" /></p>
-                    <p>取消</p>
+                    <p onClick={() => window.history.go(-1)} className='iconfont icon-zuo' style={{ width: '.7rem', textAlign: 'center' }}></p>
+                    <p className='search_p'><input type="text" /></p>
+                    <p style={{ marginRight: '.1rem' }}>取消</p>
                 </div>
-                <div className="search_main">
-                    <div><h5>历史记录</h5> <p></p></div>
+                <div className="search_box">
+                    <div className='search_sear'><h5>历史记录</h5> <p className='iconfont icon-shipin'></p></div>
                     <div>
                         <p>墨镜</p>
+                        <p>墨镜</p>
+                        <p>墨镜</p>
+                        <p>墨镜</p>
+
                     </div>
                 </div>
-                <div>
-
+                <div className='search_box_2'>
+                    <h5>热门搜索</h5>
+                    <div>
+                        <p>520元礼包先领</p>
+                    </div>
                 </div>
             </div>
         )
