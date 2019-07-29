62cfd234a843eebf3c6e833cf3dbc94b0ef72159
gengzeng <1041355591@qq.com>
2019-07-29 10:21:01 +0800

    banner 5

--- easyShop/src/components/footer/footer.js
+++ easyShop/src/components/footer/footer.js
@@ -1,16 +1,16 @@
 import React, { Component } from 'react'
-import { Link } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 import './footer.scss'
 export default class footer extends Component {
     render() {
         return (
             <div className='foot_wrap'>
                 <ul>
-                    <li> <span className='iconfont icon-iconset0124'>  </span> <Link to='/home'>首页</Link></li>
-                    <li> <span className='iconfont icon-xiazai'></span><Link to='/home'>专题</Link></li>
-                    <li> <span className='iconfont icon-fenxiang'></span><Link to='/classify'>分类</Link></li>
-                    <li> <span className='iconfont icon-iconset0124'></span><Link to='/home'>首页</Link></li>
-                    <li> <span className='iconfont icon-xingxing'></span><Link to='/home'>首页</Link></li>
+                    <li> <span className='iconfont icon-iconset0124'>  </span> <NavLink to='/home'>首页</NavLink></li>
+                    <li> <span className='iconfont icon-xiazai'></span><NavLink to='/home'>专题</NavLink></li>
+                    <li> <span className='iconfont icon-fenxiang'></span><NavLink to='/classify'>分类</NavLink></li>
+                    <li> <span className='iconfont icon-iconset0124'></span><NavLink to='/home'>首页</NavLink></li>
+                    <li> <span className='iconfont icon-xingxing'></span><NavLink to='/home'>首页</NavLink></li>
                 </ul>
 
             </div>
