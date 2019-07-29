7ec09297fdf767612d28437f4d7747b4b4624a96
gengzeng <1041355591@qq.com>
2019-07-29 09:09:25 +0800

    banner 2

--- easyShop/src/store/modules/home.js
+++ easyShop/src/store/modules/home.js
@@ -2,12 +2,37 @@ import { observable, action } from "mobx";
 import { home_All_date } from '../../server/index'
 export default class Home {
     // @observable 修饰属性
-    @observable home_date = '';
+    // @observable 修饰属性
+    @observable count = 1000;
+    //轮播数据
+    @observable banner = [];
+    //制造商数据
+    @observable brandList = [];
+    //分类数据
+    @observable categoryList = [];
+    //nav数据
+    @observable channel = [];
+    //人气推荐数据
+    @observable hotGoodsList = [];
+    //新品数据
+    @observable newGoodsList = [];
+    //专题数据
+    @observable topicList = [];
     // @action 修饰方法
-    @action home_All_date() {
-        home_All_date().then((res) => {
-            this.home_date = res
-            console.log(res)
+    @action changeCount(type) {
+        console.log('type...', type);
+        type === '+' ? this.count++ : this.count--;
+    }
+    @action getallData() {
+        home_All_date().then(res => {
+            this.banner = res.data.banner
+            this.brandList = res.data.brandList
+            this.categoryList = res.data.categoryList
+            this.hotGoodsList = res.data.hotGoodsList
+            this.channel = res.data.channel
+            this.newGoodsList = res.data.newGoodsList
+            this.topicList = res.data.topicList
+
         })
     }
 }
--- easyShop/src/views/home/home.js
+++ easyShop/src/views/home/home.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import './home.scss'
 import Swiper from 'swiper'
+import { Carousel } from "antd"
 import { inject, observer } from 'mobx-react'
 import Footer from '../../components/footer/footer'
 
@@ -12,56 +13,134 @@ class Home extends Component {
         this.state = {};
     }
     componentDidMount() {
-        this.props.home.home_All_date()
+        this.props.home.getallData()
         new Swiper(".banner", {
-            loop: true
+            loop: true,
+            pagination: {
+                el: '.swiper-pagination',
+            },
+            autoplay: true
         });
     }
     render() {
+        const { banner, brandList, channel, newGoodsList, hotGoodsList, topicList, categoryList } = this.props.home;
         return (
             <div className='home_wrap'>
-                <div className="swiper-container banner">
-                    <div className="swiper-wrapper">
-                        <div className="swiper-slide">
-                            <img src="https://yanxuan.nosdn.127.net/0b6674a2710f2ffcc63ad16a1bc2c0c7.jpg?imageView&quality=75&thumbnail=750x0" alt="" />
+                <div className="home_mains">
+                    <div className="swiper-container banner">
+                        <div className="swiper-wrapper">
+                            {
+                                banner && banner.map(item => {
+                                    return <div key={item.id} className="swiper-slide">
+                                        <img src={item.image_url} alt="" />
+                                    </div>
+                                })
+                            }
                         </div>
                     </div>
-                </div>
-                <div className="home_title">
-                    <div className="left_home">
-                        <ul>
-                            <li>推荐</li>
-                            <li>居家生活</li>
-                            <li>服饰鞋包</li>
-                            <li>美食酒水</li>
-                            <li>个护清洁</li>
-                        </ul>
+                    <div className="nav" style={{ fontSize: '.2rem' }}>
+                        {
+                            channel && channel.map(item =>
+                                <div key={item.id}><img src={item.icon_url} alt="" style={{ height: '.7rem', width: '.7rem' }} /><span>{item.name}</span></div>
+                            )
+                        }
                     </div>
-                    <div className="right_home">
-                        <p>品牌制造供应商</p>
-                        <div className='home_main'>
-                            <div>
-                                <img src="https://yanxuan.nosdn.127.net/0b6674a2710f2ffcc63ad16a1bc2c0c7.jpg?imageView&quality=75&thumbnail=750x0" alt="" />
-                            </div>
-                            <div>
-                                <img src="https://yanxuan.nosdn.127.net/0b6674a2710f2ffcc63ad16a1bc2c0c7.jpg?imageView&quality=75&thumbnail=750x0" alt="" />
-                            </div>
-                            <div>
-                                <img src="https://yanxuan.nosdn.127.net/0b6674a2710f2ffcc63ad16a1bc2c0c7.jpg?imageView&quality=75&thumbnail=750x0" alt="" />
-                            </div>
-                            <div>
-                                <img src="https://yanxuan.nosdn.127.net/0b6674a2710f2ffcc63ad16a1bc2c0c7.jpg?imageView&quality=75&thumbnail=750x0" alt="" />
-                            </div>
-                            <div>
-                                <img src="https://yanxuan.nosdn.127.net/0b6674a2710f2ffcc63ad16a1bc2c0c7.jpg?imageView&quality=75&thumbnail=750x0" alt="" />
-                            </div>
-
+                    <div className="adv">
+                        <div className="title_b">
+                            <h4>品牌制造商直供</h4>
+                        </div>
+                        <div className="bag_box">
+                            {
+                                brandList && brandList.map(item =>
+                                    <div className="box" key={item.id}>
+                                        <img src={item.list_pic_url} alt="" />
+                                        <span>{item.name}</span>
+                                    </div>
+                                )
+                            }
                         </div>
                     </div>
-                </div>
+                    <div className="newGood">
+                        <div className="title_newGood">
+                            <h4>新品首发</h4>
+                        </div>
+                        <div className="newGood_box">
+                            {
+                                newGoodsList && newGoodsList.map(item =>
+                                    <div className="sp_box" key={item.id}>
+                                        <img src={item.list_pic_url} alt="" />
+                                        <p>{item.name}</p>
+                                        <h6>￥{item.retail_price}</h6>
+                                    </div>
+                                )
+                            }
+                        </div>
+                    </div>
+                    <div className="hotList">
+                        <div className="title_hot">
+                            <h4>人气推荐</h4>
+                        </div>
+                        <div className="hotbox">
+                            {
+                                hotGoodsList && hotGoodsList.map(item =>
+                                    <div className="hotcon" key={item.id}>
+                                        <div><img src={item.list_pic_url} alt="" /></div>
+                                        <div>
+                                            <h4>{item.name}</h4>
+                                            <p>{item.goods_brief}</p>
+                                            <span>{item.retail_price}</span>
+                                        </div>
+                                    </div>
+                                )
+                            }
+                        </div>
+                    </div>
+                    <div className="scrollbox">
+                        <div className="title_hot">
+                            <h4>人气推荐</h4>
+                        </div>
 
+                        <div className="swiper-container banner">
+                            <div className="swiper-wrapper">
+                                {
+                                    topicList && topicList.map(item => {
+                                        return <div key={item.id} className="swiper-slide">
+                                            <img src={item.image_url} alt="" />
+                                        </div>
+                                    })
+                                }
+                            </div>
+                        </div>
+                    </div>
+                    <div className="cateGory">
+                        {
+                            categoryList && categoryList.map(item =>
+                                <div className="cateGoryBox" key={item.id}>
+                                    <div className="title_cateGory">
+                                        <h4>{item.name}</h4>
+                                    </div>
+                                    <div className="newGood_box">
+                                        {
+                                            item.goodsList.map(item =>
+                                                <div className="sp_box" key={item.id}>
+                                                    <img src={item.list_pic_url} alt="" />
+                                                    <p>{item.name}</p>
+                                                    <h6>￥{item.retail_price}</h6>
+                                                </div>
+                                            )
+                                        }
+                                        <div className="sp_box">
+                                            <p>更多好物</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            )
+                        }
+                    </div>
+                </div>
                 <Footer />
             </div>
+
         )
     }
 }
