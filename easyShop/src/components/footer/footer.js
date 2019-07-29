1aada027a5e21a464c9885280994d669831b34e5
gengzeng <1041355591@qq.com>
2019-07-29 12:01:06 +0800

    banner 6

--- easyShop/src/components/footer/footer.js
+++ easyShop/src/components/footer/footer.js
@@ -2,11 +2,15 @@ import React, { Component } from 'react'
 import { NavLink } from 'react-router-dom'
 import './footer.scss'
 export default class footer extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {}
+    }
     render() {
         return (
             <div className='foot_wrap'>
                 <ul>
-                    <li> <span className='iconfont icon-iconset0124'>  </span> <NavLink to='/home'>首页</NavLink></li>
+                    <li> <span className='iconfont icon-iconset0124'> </span> <NavLink to='/home'>首页</NavLink></li>
                     <li> <span className='iconfont icon-xiazai'></span><NavLink to='/home'>专题</NavLink></li>
                     <li> <span className='iconfont icon-fenxiang'></span><NavLink to='/classify'>分类</NavLink></li>
                     <li> <span className='iconfont icon-iconset0124'></span><NavLink to='/home'>首页</NavLink></li>
