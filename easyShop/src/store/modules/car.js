8d5a590f5ed106f5f259f42c7a134dd0e3944968
gengzeng <1041355591@qq.com>
2019-07-31 10:41:10 +0800

    l ok2

--- easyShop/src/store/modules/car.js
+++ easyShop/src/store/modules/car.js
@@ -4,15 +4,24 @@ import { cartIndex, address } from '../../server/index'
 export default class List {
     // @observable 修饰属性
     @observable shopping = null;
+    @observable All = null;
     money = null
     // @action 修饰方法
     @action cartIndex = async () => {
         let date = await cartIndex()
         this.money = date.data.cartTotal
         this.shopping = date.data.cartList
-
         console.log(this.money)
     }
-
-
+    @action add = (item) => {       //点击数量改变之后  还会消失
+        item.add_time++
+        item.unit_price = item.retail_price * item.add_time
+    }
+    @action del = (item) => {
+        if (item.add_time > 1) {
+            item.add_time--
+        }
+        item.unit_price = item.retail_price * item.add_time
+    }
 }
+
